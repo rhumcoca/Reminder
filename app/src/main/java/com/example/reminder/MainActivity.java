544570a31dd70f392d72f3ab90dafcb4171feecb
rhumcoca <jean_francois_tassin@hotmail.com>
2019-05-27 22:05:32 +0200

    Commit 1

--- app/src/main/java/com/example/reminder/MainActivity.java
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -97,7 +97,10 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_quit) {
+            this.onDestroy();
         }
+
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
